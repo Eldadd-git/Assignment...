f3a27cce7f8c51ba98619cedb9be27b637b896e2
Genene <me0910015@gmail.com>
2025-11-19 09:05:40 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int sum;
+    
+
+    cout << "Enter number: ";
+    cin >> a;
+
+    cout << "Enter number: ";
+    cin >> b;
+
+    sum = a + b;
+    
+
+    cout << "The sum is: " << sum << endl;
+    
+
+    return 0;
+}
